ce0bf1d9622a7d7bb1c24d8e0a6039efc3b1bba1
DankoKr <dankokralski776@gmail.com>
2024-03-31 13:08:59 +0200

    Change from Realtime db to Firestore

--- app/src/main/java/com/example/pace/CreateWorkoutActivity.kt
+++ app/src/main/java/com/example/pace/CreateWorkoutActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pace.classes.Exercise
-import com.example.pace.classes.Workout
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -16,48 +13,48 @@ import java.util.Locale
 class CreateWorkoutActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var database: DatabaseReference
+    private lateinit var firestore: FirebaseFirestore
     private lateinit var btnCreateWorkout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_workout)
 
-        // Initialize Firebase Auth and Database instances once
+        // Initialize Firebase Auth and Firestore instances
         auth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance("https://pace-6659b-default-rtdb.europe-west1.firebasedatabase.app").reference
+        firestore = FirebaseFirestore.getInstance()
 
         btnCreateWorkout = findViewById(R.id.btnCreateWorkout)
-
         btnCreateWorkout.setOnClickListener {
             saveWorkoutToFirebase()
         }
     }
 
-    fun saveWorkoutToFirebase() {
-        val userId = auth.currentUser?.uid ?: return  // Use the auth instance
+    private fun saveWorkoutToFirebase() {
+        val userId = auth.currentUser?.uid ?: return  // Get current user ID from Firebase Auth
 
         val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
 
-        val workout = Workout(
-            workoutName = "Morning Workout Test",
-            gymName = "Example Gym",
-            exercises = listOf(
-                Exercise("Push-ups", 15, 0.0),
-                Exercise("Sit-ups", 20, 10.0)
+        val workout = hashMapOf(
+            "workoutName" to "Morning Workout Test",
+            "gymName" to "Example Gym",
+            "exercises" to listOf(
+                hashMapOf("name" to "Push-ups", "reps" to 15, "kg" to 0),
+                hashMapOf("name" to "Sit-ups", "reps" to 20, "kg" to 10)
             )
         )
 
-        val userWorkoutsRef = database.child("Workouts").child(userId).child(currentDate) // Use the database instance
+        // Reference to the user document in the 'users' collection
+        val userDocRef = firestore.collection("users").document(userId)
 
-        val workoutKey = userWorkoutsRef.push().key ?: return
-
-        userWorkoutsRef.child(workoutKey).setValue(workout)
-            .addOnSuccessListener {
-                Log.d("Firebase", "Workout saved successfully")
+        // Reference to the date-specific collection within the user document
+        // Then, create a new document within this collection for the workout
+        userDocRef.collection(currentDate).add(workout)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TestFirebase", "Workout saved successfully with ID: ${documentReference.id}")
             }
             .addOnFailureListener { e ->
-                Log.w("Firebase", "Error saving workout", e)
+                Log.w("TestFirebase", "Error saving workout", e)
             }
     }
 }
